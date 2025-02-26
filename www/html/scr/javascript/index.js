e3d55ec7830c6a97388acee3bec4bd840f875825
otprotee <oscarsmessages@gmail.com>
2025-02-26 14:09:39 +0000

    1

--- www/html/scr/javascript/index.js
+++ www/html/scr/javascript/index.js
@@ -1,15 +1,11 @@
-
 function FData() {
-	document.getElementById("au").innerHTML = 'Author: Otprotee';
-	document.getElementById("con").innerHTML = 'Contributers: Me, Myself and I';
-	document.getElementById("hi").innerHTML = '<button onclick="hide()">Hide Data</button>';
-	console.log('1');
-	
+    document.getElementById("au").innerHTML = 'Author: Otprotee';
+    document.getElementById("con").innerHTML = 'Contributors: Me, Myself and I';
+    document.getElementById("hi").innerHTML = '<button onclick="hide()">Hide Data</button>';
 }
 
 function hide() { 
-	document.getElementById("au").innerHTML = 'Author: ';
-	document.getElementById("hi").innerHTML = '<button onclick="FData()">View Data</button>';
-	document.getElementById("con").innerHTML = 'Contributers:';
-	console.log('2');
+    document.getElementById("au").innerHTML = 'Author: ';
+    document.getElementById("hi").innerHTML = '<button onclick="FData()">View Data</button>';
+    document.getElementById("con").innerHTML = 'Contributors:';
 }
