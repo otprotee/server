5b8ebf03bb2a637404fd898044907d8d59bc19e4
otprotee <oscarsmessages@gmail.com>
2025-02-28 16:01:05 +0000

    2

--- www/html/scr/javascript/index.js
+++ www/html/scr/javascript/index.js
@@ -10,9 +10,3 @@ function hide() {
     document.getElementById("con").innerHTML = 'Contributors:';
 }
 
-function pictures() {
-	document.getElementById("pics").innerHTML = "<p style='background-image: url('../doc/background/ffdp.jpg'); background-size: 100% 100%; background-repeat: no-repeat; padding: 100%; background-attachment: fixed;'>"
-	setInterval(pictures, 2000)
-	document.getElementById("pics").innerHTML = "<p style='background-image: url('../doc/background/korn.jpg'); background-size: 100% 100%; background-repeat: no-repeat; padding: 100%; background-attachment: fixed;'>"
-	setInterval(pictures, 2000)
-}
